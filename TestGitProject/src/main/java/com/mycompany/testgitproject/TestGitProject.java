6969a5a869f23762c33c762435a9419cbfa634a9
Mendel <Mendel@192.168.2.17>
2023-09-19 20:21:51 -0400

    init

---
+++ TestGitProject/src/main/java/com/mycompany/testgitproject/TestGitProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testgitproject;
+
+/**
+ *
+ * @author Mendel
+ */
+public class TestGitProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
